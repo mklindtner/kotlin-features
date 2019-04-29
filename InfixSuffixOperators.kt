52272836546100260658eedf96554303c38d8377
mkl <mkl@mikiaholding.dk>
2019-04-29 14:54:10 +0200

    kt

---
+++ InfixSuffixOperators.kt
@@ -0,0 +1,51 @@
+import java.lang.Thread.sleep
+import kotlin.concurrent.thread
+
+fun main(args: Array<String>) {
+    val square = Square(5, 10)
+    val square2 = Square(5, 10)
+    println(-square)
+    println(!square)
+    println(square.times(2))
+    println(square.contains(25))
+    println(square.plus(square2))
+    println(square.neg(3))
+    println(10.Square(square))
+}
+
+
+//how to use -> Squares might need to have negative sides if we want to draw it in a coordinate system
+data class Square(val length: Int, val width: Int) {
+    var area = length * width
+
+    //member function
+    operator fun plus(other: Square ): Square {
+        return Square(other.length + this.length, this.width + other.width)
+    }
+}
+
+//extension function examples
+operator fun Square.unaryMinus() = Square(-length, -width)
+//how not to use
+operator fun Square.not() = Square((length*length) * -1, (width*width) * -1)
+//a very boring and correct example
+operator fun Square.times(b: Int) = Square(length *b, width*b)
+//a dystopian view of a Square
+operator fun Square.contains(a: Int) = Square(length, width).area <= a
+
+//infix example
+infix fun Square.neg(x: Int): Int {
+    return -(this.length) + x
+}
+
+infix fun Int.Square(x: Square): Int {
+    return this + x.length + x.width
+}
+
+
+
+
+
+
+
+
